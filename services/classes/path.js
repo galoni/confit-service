f242b07cb4e1007cf39d71c2a3def6da63dc2b69
galoni <aloni292@gmail.com |>
2018-04-02 17:45:59 +0300

    removed empty origin in yaml that broke the yaml

--- services/classes/path.js
+++ services/classes/path.js
@@ -17,6 +17,8 @@ class Path {
     this.ExploreSess = this.visitor["explore_percent"] * this.conf["sessions"].length;
 
   }
+
+  //removing parallel sessions that was added to the path to avoid redundant calculations
   removeParallelSessions(session, next) {
     console.log("removing parallel sessions to save redundant calculations:");
     for (var i = 0; i < this.conf["lectures"].length; i++) {
@@ -24,11 +26,11 @@ class Path {
         console.log("removing " + this.conf["lectures"][i]["name"]);
         this.conf["lectures"].splice(i, 1);
         i--;
-        console.log(this.conf);
       }
     }
 
   }
+  //this algorythm takes the visitor's preffered lectures and add them to the path while considering the visitor's profile pie
   insertByPref(next) {
     var lecturesLength = this.conf["lectures"].length;
     var prefferedLength = this.visitor["preffered_lectures"].length
@@ -39,7 +41,6 @@ class Path {
     }
     for (var j = 0; j < prefferedLength; j++) {
       var i = 0;
-      console.log("j = " + j);
       while (lecturesLength) {
         if (this.conf["lectures"][i] == undefined) {
           break;
@@ -81,91 +82,55 @@ class Path {
     next(null, this.build);
   }
 
-  // insertByPref(next) {
-  //   var lecturesLength = this.conf["lectures"].length;
-  //   console.log("Starting to build by the visitor's preffered Lectures");
-  //   if (this.visitor["preffered_lectures"] === undefined || this.visitor["preffered_lectures"].length == 0) {
-  //     console.log("Visitor has no preffered lectures");
-  //     next(null, build);
-  //   }
-  //   for (var i = 0; i < lecturesLength; i++) {
-  //     if (this.conf["lectures"][i] == undefined){
-  //       break;
-  //     }
-  //     if (this.visitor["preffered_lectures"].indexOf(this.conf["lectures"][i]["_id"]["$oid"]) > -1) //conf["lectures"][i]["_id"]["$oid"]){
-  //     {
-  //       try {
-  //         var indexofLec = this.program.indexOf(this.conf["lectures"][i]["session"]["session"]);
-  //         if (this.learnSess > 0){
-  //           this.build["elc_mode"][indexofLec] = 'learn';
-  //           this.learnSess -= 1;
-  //         }
-  //         else if (this.connectSess > 0){
-  //           this.build["elc_mode"][indexofLec] = 'connect';
-  //           this.connectSess -= 1;
-  //         }
-  //         else{
-  //           console.log("no slots of learn and connection left :)");
-  //           break;
-  //         }
-  //         this.build["lecture"][indexofLec] = this.conf["lectures"][i];
-  //         //need to delete all elctures form list that are in the same session
-  //         console.log("entered lecture to index: " + indexofLec);
-  //         // this.removeParallelSessions(this.program[indexofLec]);
-  //         // i=-1;
-  //       } catch (err) {
-  //         next(err);
-  //       }
-  //
-  //     }
-  //   }
-  //   next(null, this.build);
-  // }
-
+//this algorythm takes the visitor's main topics and add them to the path while considering the visitor's profile pie
   insertByTopic(next) {
     var lecturesLength = this.conf["lectures"].length;
-
+    var mainTopicLength = this.visitor["mainTopic"].length;
     console.log("Starting to build by the visitor's main topics: ");
-    if (this.visitor["mainTopic"] === undefined || this.visitor["mainTopic"].length == 0) {
+    if (this.visitor["mainTopic"] === undefined || mainTopicLength == 0) {
       console.log("Visitor has no main topics");
       next(null, build);
     }
-    //do the same as insert by preffered with the when and all
-    for (var i = 0; i < lecturesLength; i++) {
-      console.log(this.conf["lectures"][i]["name"] + "--------");
-      if (this.conf["lectures"][i] == undefined || lecturesLength == 0) {
-        break;
-      }
-      var indexofLec = this.program.indexOf(this.conf["lectures"][i]["session"]["session"]);
+    for (var i = 0; i < mainTopicLength; i++) {
+      var x = 0;
       if (this.conf["lectures"][i]["topic"] == undefined) continue;
-      for (var j = 0; j < this.conf["lectures"][i]["topic"].length; j++) {
-        console.log(this.conf["lectures"][i]["topic"][j]);
-        if (this.visitor["mainTopic"].indexOf(this.conf["lectures"][i]["topic"][j]) > -1 && (this.connectSess || this.learnSess)) //conf["lectures"][i]["_id"]["$oid"]){
-        {
-          try {
-
-            if (this.learnSess > 0) {
-              this.build["elc_mode"][indexofLec] = 'learn';
-              this.learnSess -= 1;
-            } else if (this.connectSess > 0) {
-              this.build["elc_mode"][indexofLec] = 'connect';
-              this.connectSess -= 1;
-            } else {
-              console.log("no slots of learn and connection left :)");
-              break;
-            }
-            this.build["lecture"][indexofLec] = this.conf["lectures"][i];
-            //need to delete all elctures form list that are in the same session
-            console.log("entered lecture to index: " + indexofLec);
-
-          } catch (err) {
-            next(err);
-          }
-
+      while (lecturesLength){
+        if (this.conf["lectures"][x] == undefined || lecturesLength == 0) {
+          break;
         }
-        // this.removeParallelSessions(this.program[indexofLec]);
-        // i--;
+        for (var j = 0; j < this.conf["lectures"][x]["topic"].length; j++) {
+          if (this.visitor["mainTopic"].indexOf(this.conf["lectures"][x]["topic"][j]) > -1 && (this.connectSess || this.learnSess)) //conf["lectures"][i]["_id"]["$oid"]){
+          {
+            try {
+              var indexofLec = this.program.indexOf(this.conf["lectures"][x]["session"]["session"]);
+              if (this.learnSess > 0) {
+                this.build["elc_mode"][indexofLec] = 'learn';
+                this.learnSess -= 1;
+              } else if (this.connectSess > 0) {
+                this.build["elc_mode"][indexofLec] = 'connect';
+                this.connectSess -= 1;
+              } else {
+                console.log("no slots of learn and connection left :)");
+                break;
+              }
+              this.build["lecture"][indexofLec] = this.conf["lectures"][x];
+              //need to delete all elctures form list that are in the same session
+              console.log("entered lecture to index: " + indexofLec);
+              this.removeParallelSessions(this.program[indexofLec]);
+              break;
+            } catch (err) {
+              next(err);
+            }
+
+          }
+          // this.removeParallelSessions(this.program[indexofLec]);
+          // i--;
+        }
+        if (lecturesLength==x)break;
+        x++;
       }
+
+
     }
     next(null, this.build);
   }
