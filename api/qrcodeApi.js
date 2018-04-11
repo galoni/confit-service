eea15558c15077f2d6adc6c80beb7326a3f311ec
galoni <aloni292@gmail.com |>
2018-04-12 01:13:57 +0300

    added get conf qr codes and create zip to dpownload

--- api/qrcodeApi.js
+++ api/qrcodeApi.js
@@ -3,72 +3,95 @@ var router = express.Router();
 var qrcodeService = require('../services/qrcodeService');
 var consts = require('../consts.js');
 var AWS = require('aws-sdk');
+AWS.config.update(consts.AWS_KEYS);
+var s3 = new AWS.S3();
+var fs = require('fs')
+var join = require('path').join
+var s3Zip = require('s3-zip')
+
+var config = {
+  accessKeyId: "AKIAJB2PN4H56GCR34JA",
+  secretAccessKey: "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",
+  region: "eu-central-1",
+  bucket: consts.AWS_QRCODE_BUCKET
+};
 
 router.post('/create_image_qrcode', create_image_qrcode);
 router.post('/create_string_qrcode', create_string_qrcode);
 router.post('/delete_image_qrcode', delete_image_qrcode);
 router.get('/get_image_qrcode', get_image_qrcode);
+router.post('/get_multiple_images', get_multiple_images);
 module.exports = router;
 
 //create a QR Code String and returns qr code as string
 function create_string_qrcode(req, res) {
   qrcodeService.createStringQRCode(req.body.linkedin)
-      .then(function(status) {
-          res.status(200).json({"status": status});
-      })
-      .catch(function (err) {
-          res.status(400).send(err);
+    .then(function(status) {
+      res.status(200).json({
+        "status": status
       });
+    })
+    .catch(function(err) {
+      res.status(400).send(err);
+    });
 }
 
 //create a QR Code image and returns filename
 function create_image_qrcode(req, res) {
   qrcodeService.createImagePromise(req.body.id, req.body.data, req.body.type)
-      .then(function(status) {
-          res.status(200).json({"status": status});
-      })
-      .catch(function (err) {
-          res.status(400).send(err);
+    .then(function(status) {
+      res.status(200).json({
+        "status": status
       });
+    })
+    .catch(function(err) {
+      res.status(400).send(err);
+    });
 }
 //delete a QR Code image and returns true or false
 function delete_image_qrcode(req, res) {
   qrcodeService.deleteImage(req.body.qrcode)
     .then(function(status) {
-        res.status(200).json({"file deleted": status});
+      res.status(200).json({
+        "file deleted": status
+      });
     })
-    .catch(function (err) {
-        res.status(400).send(err);
+    .catch(function(err) {
+      res.status(400).send(err);
     });
 }
 //delete a QR Code image and returns true or false
 function get_image_qrcode(req, res) {
+  if (!req.query['fileKey']) {
+    res.status(404).send('no fileKey');
+    return
+  }
   var fileKey = req.query['fileKey'];
+  console.log('Trying to download file', fileKey);
 
-    console.log('Trying to download file', fileKey);
-    AWS.config.update(consts.AWS_KEYS);
-    var s3 = new AWS.S3();
-    var options = {
-        Bucket    : consts.AWS_QRCODE_BUCKET,
-        Key    : fileKey,
-    };
+  var options = {
+    Bucket: consts.AWS_QRCODE_BUCKET,
+    Key: fileKey,
+  };
+  var fileStream = s3.getObject(options, function(err, data) {
+      if (err) {
+        console.log(err, err.stack); // an error occurred
+        res.status(404).send('bad fileKey');
+      } else console.log(data);
+    })
+    .createReadStream();
+  res.attachment(fileKey);
+  fileStream.pipe(res);
+}
 
-    res.attachment(fileKey);
-    var fileStream = s3.getObject(options).createReadStream();
-    fileStream.pipe(res);
-  // qrcodeService.getImage(req.body.qrcode, function(status){
-  //   console.log("Yellow");
-  //   if (status){
-  //     res.status(200).sendFile(status, function (err) {
-  //       if (err) {
-  //         res.status(400).send(err);
-  //       } else {
-  //         console.log('Sent:', status);
-  //       }
-  //     });
-  //   }
-  //   else{
-  //     res.status(400).json({"errmsg": "could not find image"});
-  //   }
-  // });
+function get_multiple_images(req, res) {
+  qrcodeService.get_multiple_images(req.body.confId)
+    .then(function(status) {
+      res.status(200).json({
+        "zip uploaded": status
+      });
+    })
+    .catch(function(err) {
+      res.status(400).send(err);
+    });
 }
