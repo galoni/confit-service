9dad5f3a39d7b98bddb16283757be1ecb40e3747
nofarginon <nofar.ginon1@gmail.com>
2018-04-11 21:15:34 +0300

    'retriveDataFixLife'

--- consts.js
+++ consts.js
@@ -2,9 +2,9 @@ module.exports = {
   MLAB_KEY: `mongodb://NivoB:Nb123456@ds223578.mlab.com:23578/confit`,
   QRCODELIB: 'qr_images/',
   AWS_KEYS: {
-    accessKeyId: "AKIAIHLMXQZOZ4OEVNNA",
-    secretAccessKey: "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"
-  },
+      accessKeyId: "AKIAJB2PN4H56GCR34JA",
+      secretAccessKey: "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"
+    },
   AWS_QRCODE_BUCKET : 'qr-code-storage',
   AWS_LOGO_BUCKET: 'logo-storage'
 }
